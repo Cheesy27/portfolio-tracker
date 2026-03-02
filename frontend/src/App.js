33e7235a3ba8af311f6daa319186e8fe741f1312
Cheesy27 <lucasjan27@gmail.com>
2026-03-02 11:21:22 -0500

    PT-13: add holding form connected to backend API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import HoldingsList from "./components/HoldingsList";
+import AddHoldingForm from "./components/AddHoldingForm";
 
 function App() {
+  const [refresh, setRefresh] = useState(0);
+
+  const handleHoldingAdded = () => {
+    setRefresh(refresh + 1);
+  };
+
   return (
     <div>
       <h1>Portfolio Tracker</h1>
+      <AddHoldingForm onHoldingAdded={handleHoldingAdded} />
+      <HoldingsList refresh={refresh} />
     </div>
   );
 }
