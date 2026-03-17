e6eaee9fad39ca73458d89e7f6524a9935363ea2
Cheesy27 <lucasjan27@gmail.com>
2026-03-16 21:06:07 -0400

    PT-24: Migrate from H2 to Oracle XE via Docker

--- backend/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO holdings (symbol, name, quantity, purchase_price) VALUES ('AAPL', 'Apple Inc', 10, 264.72);
-INSERT INTO holdings (symbol, name, quantity, purchase_price) VALUES ('GOOG', 'Alphabet Inc', 5, 306.36);
-INSERT INTO holdings (symbol, name, quantity, purchase_price) VALUES ('FBAL', 'Fidelity All-in-One Balanced ETF', 50, 15.01);
-INSERT INTO holdings (symbol, name, quantity, purchase_price) VALUES ('FCNS', 'Fidelity All-in-One Conservative ETF', 30, 12.56);
-INSERT INTO holdings (symbol, name, quantity, purchase_price) VALUES ('FGRO', 'Fidelity All-in-One Growth ETF', 25, 18.04);
